34593b03da5b87ba98a4e09b57c54eee09a66358
Addisu87 <addisuhaile87@gmail.com>
2021-10-05 15:38:08 +0300

    ADD: redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,58 +1,67 @@
-import { findByLabelText } from '@testing-library/dom'
-import React from 'react'
-import styled from "styled-components"
+import { findByLabelText } from "@testing-library/dom";
+import React from "react";
+import styled from "styled-components";
+import { Fade } from "react-awesome-reveal";
 
-function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
+function Section({
+    title,
+    description,
+    leftBtnText,
+    rightBtnText,
+    backgroundImg
+}) {
     return (
         <Wrap bgImage={backgroundImg}>
-          <ItemText>
-              <h1>{title}</h1>
-              <p>{description}</p>
-          </ItemText>
-          <Buttons>
-            <ButtonGroup>
-                <LeftButton>
-                    {leftBtnText}
-                </LeftButton>
-                {rightBtnText &&
-                    <RightButton>
-                        {rightBtnText}
-                    </RightButton>
-                }
-                
-            </ButtonGroup>
-            <DownArrow src="/images/down-arrow.svg"/> 
-          </Buttons> 
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
+            <Buttons>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>{leftBtnText}</LeftButton>
+                        {rightBtnText && (
+                            <RightButton>{rightBtnText}</RightButton>
+                        )}
+                    </ButtonGroup>
+                </Fade>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
-    )
+    );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background-size: cover; 
-    background-position: center; 
-    background-repeat: no-repeat; 
-    background-image: url('/images/model-s.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url("/images/model-s.jpg");
     display: flex;
     flex-direction: column;
-    justify-content: space-between;  // vertical 
+    justify-content: space-between; // vertical
     align-items: center; // horizontal
-    background-image: ${props => `url("/images/${props.bgImage}")`}
-`
+    background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
-`
+    z-index: -1;
+`;
+
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
     @media (max-width: 768px) {
         flex-direction: column;
     }
-`
+`;
 
 const LeftButton = styled.div`
     background-color: rgba(23, 26, 32, 0.8);
@@ -68,21 +77,18 @@ const LeftButton = styled.div`
     font-size: 12px; 
     cursor: pointer;
     margin= 8px; 
-`
+`;
 
 const RightButton = styled(LeftButton)`
     background: white;
     opacity: 0.65;
     color: black;
-`
+`;
 
 const DownArrow = styled.img`
     height: 40px;
-    overflow-x: hidden;  
-    animation: animateDown infinite 1.5s; 
-`
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`;
 
-const Buttons = styled.div`
-
-`
-
+const Buttons = styled.div``;
